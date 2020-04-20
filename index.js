958fed66736c26aaaf40a2cbaf6d819bf410d90f
Max Jacobson <mjacobson7@gmail.com>
2020-04-20 11:10:43 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post('/max-test', (req, res) => {
+  console.log(req.body)
+})
+
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080')
+})
