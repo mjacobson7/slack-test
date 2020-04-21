9fe867ad3862e2c1b422a4339f100f019c9a0bcb
Max Jacobson <mjacobson7@gmail.com>
2020-04-21 11:43:39 -0600

    commit

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
+const axios = require('axios')
+require('dotenv').config()
 const app = express();
+
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -17,56 +20,75 @@ app.get('/load', (req, res) => {
 
 app.post('/max-test', (req, res) => {
   console.log(req.body)
-  res.status(200).json(
-    {
-      "channel": "C1H9RESGL",
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "Danny Torrence left the following review for your property:"
-          }
-        },
-        {
-          "type": "section",
-          "block_id": "section567",
-          "text": {
-            "type": "mrkdwn",
-            "text": "<https://google.com|Overlook Hotel> \n :star: \n Doors had too many axe holes, guest in room 237 was far too rowdy, whole place felt stuck in the 1920s."
-          },
-          "accessory": {
-            "type": "image",
-            "image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
-            "alt_text": "Haunted hotel image"
-          }
-        },
-        {
-          "type": "section",
-          "block_id": "section789",
-          "fields": [
-            {
-              "type": "mrkdwn",
-              "text": "*Average Rating*\n1.0"
-            }
-          ]
-        },
-        {
-          "type": "actions",
-          "elements": [
-            {
-              "type": "button",
-              "text": {
-                "type": "plain_text",
-                "text": "Reply to review",
-                "emoji": false
-              }
-            }
-          ]
-        }
-      ]
-    }
-  )
+
+  const data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: '#test',
+    text: "Hi! :wave: \n I'm your new bot."
+  }}
+
+  axios.post('https://slack.com/api/chat.postMessage', data).then(res => {
+    res.json();
+  })
+
+
+  // axios.get('https://slack.com/api/users.identity', data).then(res => {
+  //   console.log(res.data)
+  // })
+
+  // res.status(200).json(
+  //   {
+  //     "channel": "C1H9RESGL",
+  //     "blocks": [
+  //       {
+  //         "type": "section",
+  //         "text": {
+  //           "type": "mrkdwn",
+  //           "text": "Who would you like to recognize?"
+  //         }
+  //       },
+  //       {
+  //         "type": "section",
+  //         "block_id": "section567",
+  //         "text": {
+  //           "type": "mrkdwn",
+  //           "text": "<https://google.com|Overlook Hotel> \n :star: \n Doors had too many axe holes, guest in room 237 was far too rowdy, whole place felt stuck in the 1920s."
+  //         },
+  //         "accessory": {
+  //           "type": "image",
+  //           "image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
+  //           "alt_text": "Haunted hotel image"
+  //         }
+  //       },
+  //       {
+  //         "type": "section",
+  //         "block_id": "section789",
+  //         "fields": [
+  //           {
+  //             "type": "mrkdwn",
+  //             "text": "*Average Rating*\n1.0"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         "type": "actions",
+  //         "elements": [
+  //           {
+  //             "type": "button",
+  //             "text": {
+  //               "type": "plain_text",
+  //               "text": "Reply to review",
+  //               "emoji": false
+  //             }
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // )
+
+
+
 })
 
 // app.use('/index.html', express.static(__dirname + '/index.html'))
