7948abd03d462115c0cf931cef99de85aa1cccd3
Max Jacobson <mjacobson7@gmail.com>
2020-04-22 10:44:43 -0600

    commit

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ app.get('/load', (req, res) => {
   res.status(200).json('Server has loaded!')
 })
 
-app.post('/max-test', async (req, res) => {
+app.post('/slack-recognize', async (req, res) => {
   try {
-
-
 
     const { trigger_id: triggerId } = req.body;
 
@@ -74,94 +72,20 @@ app.post('/max-test', async (req, res) => {
       },
     });
 
-
-    // console.log(req.body)
-    // if (req.body.payload) {
-    //   const payload = JSON.parse(req.body.payload)
-    //   console.log(payload.callback_id)
-
-    //   if(payload.callback_id == 'user_selection') {
-    //     const responseObj = {
-    //       text: "Choose a program",
-    //       attachments: [
-    //         {
-    //           color: "#3AA3E3",
-    //           attachment_type: "default",
-    //           callback_id: "program_selection",
-    //           actions: [
-    //             {
-    //               name: "program_list",
-    //               text: "Select a Program",
-    //               type: "select",
-    //               options: [
-    //                 {
-    //                   text: 'Anniversary Program',
-    //                   value: 'ANNIVERSARY'
-    //                 },
-    //                 {
-    //                   text: 'Happy Birthday',
-    //                   value: 'BIRTHDAY'
-    //                 },
-    //                 {
-    //                   text: 'High Five',
-    //                   value: 'HF'
-    //                 }
-    //               ]
-    //             }
-    //           ]
-    //         }
-    //       ]
-    //     }
-
-    //     res.status(200).json(responseObj)
-
-    //   }
-
-
-
-
-    // } else {
-    //   const response = await axios.get('https://slack.com/api/users.list?token=' + process.env.SLACK_AUTH_TOKEN)
-    //   const users = response.data.members;
-    //   const options = [];
-
-    //   users.map(user => {
-    //     if (!user.deleted && !user.is_bot && user.real_name != 'Slackbot') {
-    //       options.push({
-    //         text: user.real_name,
-    //         value: user.id
-    //       })
-    //     }
-    //   })
-
-
-    //   const responseObj = {
-    //     text: "Who would you like to recognize?",
-    //     attachments: [
-    //       {
-    //         color: "#3AA3E3",
-    //         attachment_type: "default",
-    //         callback_id: "user_selection",
-    //         actions: [
-    //           {
-    //             name: "users_list",
-    //             text: "Select a user",
-    //             type: "select",
-    //             options: options
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   }
-
-    //   res.status(200).json(responseObj)
-    // }
-
   } catch (e) {
     console.log(e)
     res.status(500).send(e)
   }
 
+})
+
+
+app.post('/slack-user', async (req, res) => {
+  try {
+    console.log('We did it!')
+  } catch(e) {
+    console.log(e)
+  }
 })
 
 
