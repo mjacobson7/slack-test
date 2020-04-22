783a5f26ad7215cba16e08108acd8252fbacb5cc
Max Jacobson <mjacobson7@gmail.com>
2020-04-22 09:37:56 -0600

    commit

--- index.js
+++ index.js
@@ -19,87 +19,155 @@ app.get('/load', (req, res) => {
 
 app.post('/max-test', async (req, res) => {
   try {
-    console.log(req.body)
-    if (req.body.payload) {
-      const payload = JSON.parse(req.body.payload)
-      console.log(payload.callback_id)
 
-      if(payload.callback_id == 'user_selection') {
-        const responseObj = {
-          text: "Choose a program",
-          attachments: [
+
+
+    const { trigger_id: triggerId } = req.body;
+
+    res.status(200).send('');
+    (async () => {
+      // Open a modal.
+      await web.views.open({
+        trigger_id: triggerId,
+        view: {
+          type: 'modal',
+          title: {
+            type: 'plain_text',
+            text: 'Contact Front Desk',
+          },
+          submit: {
+            type: 'plain_text',
+            text: 'Submit',
+          },
+          callback_id: 'frontdesk',
+          blocks: [
             {
-              color: "#3AA3E3",
-              attachment_type: "default",
-              callback_id: "program_selection",
-              actions: [
-                {
-                  name: "program_list",
-                  text: "Select a Program",
-                  type: "select",
-                  options: [
-                    {
-                      text: 'Anniversary Program',
-                      value: 'ANNIVERSARY'
-                    },
-                    {
-                      text: 'Happy Birthday',
-                      value: 'BIRTHDAY'
-                    },
-                    {
-                      text: 'High Five',
-                      value: 'HF'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
+              type: 'section',
+              text: {
+                type: 'plain_text',
+                text: ':wave: We will get back to you as soon as possible',
+                emoji: true,
+              },
+            },
+            {
+              type: 'divider',
+            },
+  
+            {
+              type: 'input',
+              block_id: 'title',
+              label: {
+                type: 'plain_text',
+                text: 'Title',
+                emoji: true,
+              },
+              element: {
+                type: 'plain_text_input',
+                multiline: false,
+                action_id: 'title',
+              },
+            },
+            {
+              type: 'input',
+              block_id: 'description',
+              label: {
+                type: 'plain_text',
+                text: 'Description',
+                emoji: true,
+              },
+              element: {
+                type: 'plain_text_input',
+                multiline: true,
+                action_id: 'description',
+              },
+              optional: true,
+            },
+          ],
+        },
+      });
+    })();
+    
+    // console.log(req.body)
+    // if (req.body.payload) {
+    //   const payload = JSON.parse(req.body.payload)
+    //   console.log(payload.callback_id)
 
-        res.status(200).json(responseObj)
+    //   if(payload.callback_id == 'user_selection') {
+    //     const responseObj = {
+    //       text: "Choose a program",
+    //       attachments: [
+    //         {
+    //           color: "#3AA3E3",
+    //           attachment_type: "default",
+    //           callback_id: "program_selection",
+    //           actions: [
+    //             {
+    //               name: "program_list",
+    //               text: "Select a Program",
+    //               type: "select",
+    //               options: [
+    //                 {
+    //                   text: 'Anniversary Program',
+    //                   value: 'ANNIVERSARY'
+    //                 },
+    //                 {
+    //                   text: 'Happy Birthday',
+    //                   value: 'BIRTHDAY'
+    //                 },
+    //                 {
+    //                   text: 'High Five',
+    //                   value: 'HF'
+    //                 }
+    //               ]
+    //             }
+    //           ]
+    //         }
+    //       ]
+    //     }
 
-      }
+    //     res.status(200).json(responseObj)
+
+    //   }
 
 
 
 
-    } else {
-      const response = await axios.get('https://slack.com/api/users.list?token=' + process.env.SLACK_AUTH_TOKEN)
-      const users = response.data.members;
-      const options = [];
+    // } else {
+    //   const response = await axios.get('https://slack.com/api/users.list?token=' + process.env.SLACK_AUTH_TOKEN)
+    //   const users = response.data.members;
+    //   const options = [];
 
-      users.map(user => {
-        if (!user.deleted && !user.is_bot && user.real_name != 'Slackbot') {
-          options.push({
-            text: user.real_name,
-            value: user.id
-          })
-        }
-      })
+    //   users.map(user => {
+    //     if (!user.deleted && !user.is_bot && user.real_name != 'Slackbot') {
+    //       options.push({
+    //         text: user.real_name,
+    //         value: user.id
+    //       })
+    //     }
+    //   })
 
 
-      const responseObj = {
-        text: "Who would you like to recognize?",
-        attachments: [
-          {
-            color: "#3AA3E3",
-            attachment_type: "default",
-            callback_id: "user_selection",
-            actions: [
-              {
-                name: "users_list",
-                text: "Select a user",
-                type: "select",
-                options: options
-              }
-            ]
-          }
-        ]
-      }
+    //   const responseObj = {
+    //     text: "Who would you like to recognize?",
+    //     attachments: [
+    //       {
+    //         color: "#3AA3E3",
+    //         attachment_type: "default",
+    //         callback_id: "user_selection",
+    //         actions: [
+    //           {
+    //             name: "users_list",
+    //             text: "Select a user",
+    //             type: "select",
+    //             options: options
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   }
 
-      res.status(200).json(responseObj)
-    }
+    //   res.status(200).json(responseObj)
+    // }
 
   } catch (e) {
     console.log(e)
